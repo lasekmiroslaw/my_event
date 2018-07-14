68092fadf053854f9faea97355140a2657c324a0
Miro <lasekmiroslaw@gmail.com>
2018-07-14 20:16:53 +0200

    login method stub

---
+++ symfony/src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+}
