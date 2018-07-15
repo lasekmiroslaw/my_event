15d694d2ca1bb7f0361664881d2a07fbacdef628
Miro <lasekmiroslaw@gmail.com>
2018-07-15 12:37:30 +0200

    add password hash listener

---
+++ symfony/src/EventListener/PasswordHashListener.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miro
+ * Date: 7/15/18
+ * Time: 12:34 PM
+ */
+
+namespace App\EventListener;
+
+
+class PasswordHashListener
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        // only act on some "User" entity
+        if (!$entity instanceof User) {
+            return;
+        }
+        $password = $this->passwordEncoder->encodePassword($entity, $entity->getPlainPassword());
+        $entity->setPassword($password);
+    }
+}
